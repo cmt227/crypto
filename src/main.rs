dd6e97a015b8ab8769115d4fc54bdd516e4f2bc1
Carsten Thue-Bludworth <cmt227@cornell.edu>
2015-07-08 16:42:42 -0400

    Implimented challenge 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate rustc_serialize;
-use rustc_serialize::hex::FromHex;
+use rustc_serialize::hex::ToHex;
 use rustc_serialize::base64::*;
 
 use std::env;
@@ -9,9 +9,17 @@ fn main() {
     if args.len() < 2 {
         panic!("no argument to convert :(");
     }
+    /* challenge 1 
     let s: String = args[1].clone();
     let res = base64_of_hex(s);
     println!("Program successful.\nResult: {}", res);
+    */
+    let s1: String = args[1].clone();
+    let s2: String = args[2].clone();
+    let bytes1 = s1.bytes_of_hex();
+    let bytes2 = s2.bytes_of_hex();
+    let res = fixed_xor(bytes1, bytes2).to_hex();
+    println!("Program successful!\nResult: {}", res);
 }
 
 fn base64_of_hex(hex: String) -> String {
@@ -22,15 +30,27 @@ fn base64_of_hex(hex: String) -> String {
         line_length: None,
     };
 
-    let bytes = hex.from_hex().unwrap();
+    let bytes = hex.bytes_of_hex();
     bytes.to_base64(config)
 }
-/*
+
+// Precondition: bytes1 and bytes2 are of equal lengths.
+fn fixed_xor(bytes1: Vec<u8>, bytes2: Vec<u8>) -> Vec<u8> {
+    if bytes1.len() != bytes2.len() {
+        panic!("Unequal length arguments!");
+    }
+    let mut res = Vec::with_capacity(bytes1.len());
+    for i in 0..(bytes1.len()) {
+        res.push(bytes1[i] ^ bytes2[i]);
+    }
+    res
+}
+
 trait HexConverter {
     fn bytes_of_hex(&self) -> Vec<u8>;
 }
 
-impl HexConverter for str {
+impl HexConverter for String {
     fn bytes_of_hex(&self) -> Vec<u8> {
         let mut res = Vec::with_capacity(self.len() / 2);
         let mut modulus = 0;
@@ -53,6 +73,6 @@ impl HexConverter for str {
                 res.push(buf);
             }
         }
+        res
     }
 }
-*/
